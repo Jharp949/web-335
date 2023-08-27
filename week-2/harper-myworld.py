4293a735a30622421e78a49b2b5f86fbc33efa35
James Harper <jharp949@gmail.com>
2023-08-27 09:03:08 -0700

    Changes to be committed: 	modified:   week-2/harper-myworld.py 	deleted:    week-3/harper.css 	deleted:    week-3/harper.html 	deleted:    week-3/harper.js

--- week-2/harper-myworld.py
+++ week-2/harper-myworld.py
@@ -1,1 +1,8 @@
+"""
+    Title: harper-myworld.py
+    Author: James Harper
+    Date: 11 August 2023
+    Description: Example file for creating methods in Python.
+"""
+
 print("You are now in Harper's world!")
