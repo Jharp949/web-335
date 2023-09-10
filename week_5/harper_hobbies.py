0fb70ef476d96e710b13a6ffb0ca1bdd9e95f107
James Harper <jharp949@gmail.com>
2023-09-09 22:04:36 -0700

    Update harper_hobbies.py

--- week_5/harper_hobbies.py
+++ week_5/harper_hobbies.py
@@ -5,13 +5,17 @@
     Description: Python Conditionals, Lists, and Loops.
 """
 
+#List containing 5 hobbies. 
 hobbies = ["Moutain Biking", "Brewing", "Gaming", "Wood Working", "Coding"]
 
+#Loop that iterates through each item in the hobbies list.
 for item in hobbies:
     print(item)
-    
+
+#List of days of the week.     
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+#Loop through each item in days, prints the day to the terminal with a custom message.
 for item in days:
     if item == "Sunday" or item == "Saturday":
         print(str(item) + ". Day off! Time to enjoy some hobbies.")
